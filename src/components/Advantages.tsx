924f4faaf320b90df91edbe8e0d6be9a50838e78
unknown <jmiguelafonso@hotmail.com>
2024-07-05 00:55:47 -0300

    hero and advantages

--- src/components/Advantages.tsx
+++ src/components/Advantages.tsx
@@ -1,15 +1,39 @@
-export function Advantages(){
-return(
+const arrAdvantages = [
+ {
+  icon: "../src/assets/images/icons/icon3.png",
+  title: "+ de 250",
+  subtitle: "restaurantes parceiros",
+ },
+ {
+  icon: "../src/assets/images/icons/icon1.png",
+  title: "+ de R$ 7.000,00",
+  subtitle: "em descontos",
+ },
+ {
+  icon: "../src/assets/images/icons/icon2.png",
+  title: "+ de 100 km",
+  subtitle: "para você conhecer",
+ },
+];
+
+export function Advantages() {
+ return (
   <section className="h-[296px]">
-    <div className="w-full h-full bg-[#1f331e] ">
-      <div className="container mx-auto pt-10">
-        <h1 className="font-bold text-[#6aed64] text-2xl">vantages</h1>
-        <p className="text-[#6aed64]">textosss</p>
+   <div className="w-full h-full bg-[#1f331e] ">
+    <div className="container mx-auto w-full  lg:flex justify-around lg:gap-x-8 pt-10">
+     {arrAdvantages.map((item)=>{
+      return(
+        <div className="flex flex-col items-center justify-center gap-1 ">
+          <img src={item.icon} className="w-[78px] h-[78px]" />
+          <h1 className="font-bold text-[50px] text-[#6aed64] mt-2">{item.title}</h1>
+          <h2 className="text-[20px] text-[#64ed8d]">{item.subtitle}</h2>
+        </div>
+      )
+     })
 
-      </div>
+     }
     </div>
-
+   </div>
   </section>
-)  
-
+ );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,41 @@
-export function Hero(){
-  return(
-    <section className="h-screen">
-   <div className="w-full h-full ">
-   <p>background image</p>
-    <div className="container w-full border m-auto mt-24">
-    <h1>infooo</h1>
+import { Button } from "./Button/Button";
+import { Logo } from "./Logo/Logo";
 
+export function Hero() {
+ return (
+  <section className=" h-[1104px] overflow-hidden ">
+   <div className="w-full h-full bg-cover flex  bg-center bg-[url('../src/assets/images/bg.jpeg')]">
+    <div className="container mx-auto   mt-24 flex   ">
+     <div className="w-[580px]">
+      <div className="flex">
+       <h1 className=" text-4xl uppercase text-[25px] font-bold text-[#6aed64] px-6 py-2 rounded-2xl bg-[#1f331e] ">
+        Economize no melhor da região
+       </h1>
+      </div>
+      <div className="text-left px-2 text-[#6aed64]">
+       <p className="mt-8 text-[40px]  ">
+        Seu <span className="text-[48px] font-bold"> guia completo</span> para{" "}
+        conhecer os melhores lugares da{" "}
+        <span className="text-[48px] font-bold">Região dos Lagos - RJ</span>
+       </p>
+       <p className="mt-10 text-[20px]">
+        Seja você um{" "}
+        <span className="font-bold">
+         turista em busca de experiências únicas
+        </span>{" "}
+        ou um <span className="font-bold">morador em busca de novidades</span>,
+        o <span className="font-bold">app Facilita Lagos</span> é a sua chave
+        para ter acesso aos locais turísticos mais requisitados da região e tudo
+        isso <span className="font-bold">com descontos exclusivos.</span>
+       </p>
+       <Button cn="mt-20" />
+      </div>
+     </div>
+     <div>
+      <Logo />
+     </div>
     </div>
    </div>
-      
-    </section>
-  )
+  </section>
+ );
 }
